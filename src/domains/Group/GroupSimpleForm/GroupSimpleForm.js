249ef22e88c58158d53ea8ed6aa21a8845c8ab23
jazzkrop <jazzkrop@gmail.com>
2021-11-30 00:05:28 +0200

    group simple form

--- src/domains/Group/GroupSimpleForm/GroupSimpleForm.js
+++ src/domains/Group/GroupSimpleForm/GroupSimpleForm.js
@@ -1,4 +1,67 @@
+import React from 'react'
+import { Form, Select, DatePicker } from 'antd'
+import {
+  Container,
+  Row,
+  Col,
+  Input,
+  Button,
+  // Select,
+  Title,
+  TextArea
+} from '@qonsoll/react-design'
+
 const GroupSimpleForm = (props) => {
-  return <div>GroupSimpleForm</div>
+  return (
+    <Form layout="vertical">
+      <Container>
+        <Row mb={3}>
+          <Col>
+            <Title level={5}>Main information</Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Item name="name" label="Name">
+              <Input placeholder="Enter name" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Item name="curator" label="Curator">
+              <Select placeholder="Select curator">
+                <Select.Option value="ID_OF_CURATOR_1">
+                  Name of curator 1
+                </Select.Option>
+
+                <Select.Option value="ID_OF_CURATOR_2">
+                  Name of curator 2
+                </Select.Option>
+
+                <Select.Option value="ID_OF_CURATOR_3">
+                  Name of curator 3
+                </Select.Option>
+
+                <Select.Option value="ID_OF_CURATOR_4">
+                  Name of curator 4
+                </Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row h="right">
+          <Col cw="auto">
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Save
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
+  )
 }
 export default GroupSimpleForm
--- src/pages/App/Groups/GroupCreate.js
+++ src/pages/App/Groups/GroupCreate.js
@@ -1,8 +1,11 @@
 import { PageWrapper } from '@qonsoll/react-design'
+import GroupSimpleForm from '../../../domains/Group/GroupSimpleForm/GroupSimpleForm'
 
 const GroupCreate = (props) => {
   return (
-    <PageWrapper headingProps={{ title: 'Create group' }}>content</PageWrapper>
+    <PageWrapper headingProps={{ title: 'Create group' }}>
+      <GroupSimpleForm />
+    </PageWrapper>
   )
 }
 export default GroupCreate
