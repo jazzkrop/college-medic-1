addb4fe87274a687ee6241319b17c150bf8d9e58
jazzkrop <jazzkrop@gmail.com>
2021-11-29 14:57:07 +0200

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,38 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "@firebase/firestore";
+import { useNavigate } from 'react-router-dom'
+import app from "services";
+
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+const db = getFirestore(app)
+
 const Login = (props) => {
-  return <div>Login</div>
+  const navigate = useNavigate()
+
+  const login = async () => {
+    const result = await signInWithPopup(auth, provider)
+    const user = result.user
+    const userRef = doc(db, 'users', user.uid)
+    const userSnapshot = await getDoc(userRef)
+    if (!userSnapshot.exists()) {
+      await setDoc(doc(db, 'users', user.uid), {
+        id: user.uid,
+        firstName: user.displayName || null,
+        avatarUrl: user.photoURL || null
+      })
+      navigate(`/users/${user.uid}/edit`)
+    }
+      navigate(`/users/${user.uid}/edit`)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <button onClick={login}>Login or Sign Up</button>
+      </div>
+    </div>
+  )
 }
 export default Login
